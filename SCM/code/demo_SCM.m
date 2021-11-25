a0a13c0338eb0c2b0577aa57b43f3667df6672d8
FutureTwT <1131797361@qq.com>
2021-11-25 09:23:46 +0800

    format SCM

--- SCM/code/demo_SCM.m
+++ SCM/code/demo_SCM.m
@@ -1,16 +1,17 @@
-
-function [  ] = demo_SCM(bits, dataname)
+function [] = demo_SCM(bits, dataname)
     lambda = 1e-6;
     data_dir = '../../Data';
-    if ~exist(data_dir,'dir')
+
+    if ~exist(data_dir, 'dir')
         error('No such dir(%s)', fullfile(pwd, data_dir))
     end
-    if ~exist('../result','dir')
+
+    if ~exist('../result', 'dir')
         mkdir('../result')
     end
 
     addpath(data_dir);
-	bits = str2num(bits);
+    bits = str2num(bits);
 
     if strcmp(dataname, 'flickr')
         load('mir_cnn.mat');
@@ -27,8 +28,8 @@ function [  ] = demo_SCM(bits, dataname)
     YTrain = I_tr;
     clear T_tr I_tr;
 
-    XTest  = T_te;
-    YTest  = I_te;
+    XTest = T_te;
+    YTest = I_te;
     clear T_te I_te;
 
     XDb = T_db;
@@ -46,13 +47,12 @@ function [  ] = demo_SCM(bits, dataname)
     YTrain = bsxfun(@minus, YTrain, YMean);
     YTest = bsxfun(@minus, YTest, YMean);
 
-    Cxx = XTrain'*XTrain + lambda*eye(dimX);
-    Cyy = YTrain'*YTrain + lambda*eye(dimY);
-    Cxy = XTrain'*YTrain;
+    Cxx = XTrain' * XTrain + lambda * eye(dimX);
+    Cyy = YTrain' * YTrain + lambda * eye(dimY);
+    Cxy = XTrain' * YTrain;
 
     % evaluation
     WTrue = (L_te * L_db' >= 1);
-
 
     [SCMsWx, SCMsWy] = trainSCM_seq(XTrain, YTrain, bits, L_tr);
 
@@ -66,10 +66,10 @@ function [  ] = demo_SCM(bits, dataname)
     hamm_I2T = hammingDist(ByTest, BxDb)';
     MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
 
-	name = ['../result/' dataname '.txt'];
+    name = ['../result/' dataname '.txt'];
     fid = fopen(name, 'a+');
     fprintf(fid, '[%s-%d] MAP@I2T = %.4f, MAP@T2I = %.4f\n', dataname, bits, MAP_I2T, MAP_T2I);
-	fclose(fid);
+    fclose(fid);
 
 end
 
@@ -78,12 +78,12 @@ function cb = compactbit(b)
     % b = bits array
     % cb = compacted string of bits (using words of 'word' bits)
     [nSamples nbits] = size(b);
-    nwords = ceil(nbits/8);
+    nwords = ceil(nbits / 8);
     cb = zeros([nSamples nwords], 'uint8');
 
     for j = 1:nbits
-        w = ceil(j/8);
-        cb(:,w) = bitset(cb(:,w), mod(j-1,8)+1, b(:,j));
+        w = ceil(j / 8);
+        cb(:, w) = bitset(cb(:, w), mod(j - 1, 8) + 1, b(:, j));
     end
-end
 
+end
--- SCM/code/trainSCM_orth.m
+++ SCM/code/trainSCM_orth.m
@@ -1,39 +1,38 @@
-function [ Wx, Wy ] = trainSCM_orth(X, Y, nDim, L)
-% [Wx, Wy] = trainSCM_orth(X, Y, nDim, L)
-% Computes the Semantic Correlation Maximization orthogonal projection 
-% on two data matrices X and Y.
-%
-% Input:
-% X - data matrix of modality x
-% Y - data matrix of modality y
-% nDim - number of SCM-orth dimensions
-% L - label matrix
-%
-% Output:
-% Wx - SCM-orth projection matrix for modality X
-% Wy - SCM-orth projection matrix for modality Y
+function [Wx, Wy] = trainSCM_orth(X, Y, nDim, L)
+    % [Wx, Wy] = trainSCM_orth(X, Y, nDim, L)
+    % Computes the Semantic Correlation Maximization orthogonal projection
+    % on two data matrices X and Y.
+    %
+    % Input:
+    % X - data matrix of modality x
+    % Y - data matrix of modality y
+    % nDim - number of SCM-orth dimensions
+    % L - label matrix
+    %
+    % Output:
+    % Wx - SCM-orth projection matrix for modality X
+    % Wy - SCM-orth projection matrix for modality Y
 
-nData = size(X, 1);
-dX = size(X, 2);
-dY = size(Y, 2);
-lambda = 1e-6;
+    nData = size(X, 1);
+    dX = size(X, 2);
+    dY = size(Y, 2);
+    lambda = 1e-6;
 
-L(L <= 0) = 0;
-L(L > 0) = 1;
+    L(L <= 0) = 0;
+    L(L > 0) = 1;
 
-% normalize the label matrix
-Length = sqrt(sum(L.^2, 2));
-Length(Length <= 0) = 1e-8; % avoid division by zero problem for unlabeled rows
-Lambda = 1 ./ Length;
-L = diag(sparse(Lambda)) * L;
+    % normalize the label matrix
+    Length = sqrt(sum(L.^2, 2));
+    Length(Length <= 0) = 1e-8; % avoid division by zero problem for unlabeled rows
+    Lambda = 1 ./ Length;
+    L = diag(sparse(Lambda)) * L;
 
-CxyB = 2*(X'*L)*(Y'*L)' - (X'*ones(nData, 1))*(Y'*ones(nData, 1))';
-Cxy = (nDim * 1) * CxyB;
-    
-Cxx = X'*X + lambda*eye(dX);
-Cyy = Y'*Y + lambda*eye(dY);
+    CxyB = 2 * (X' * L) * (Y' * L)' - (X' * ones(nData, 1)) * (Y' * ones(nData, 1))';
+    Cxy = (nDim * 1) * CxyB;
 
-[Wx, Wy, eigv] = trainCCA(Cxx, Cyy, Cxy, nDim);
+    Cxx = X' * X + lambda * eye(dX);
+    Cyy = Y' * Y + lambda * eye(dY);
+
+    [Wx, Wy, eigv] = trainCCA(Cxx, Cyy, Cxy, nDim);
 
 end
-
