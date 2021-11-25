b7221101a09a8420df6a64aac86ec0e35ef9f706
FutureTwT <1131797361@qq.com>
2021-11-25 09:27:09 +0800

    format SMFH

--- SMFH/code/demo_SMFH.m
+++ SMFH/code/demo_SMFH.m
@@ -1,16 +1,18 @@
-
 function [] = demo_SMFH(bits, dataname)
-	bits = str2num(bits);
+    bits = str2num(bits);
 
     data_dir = '../../Data';
-    if ~exist(data_dir,'dir')
+
+    if ~exist(data_dir, 'dir')
         error('No such dir(%s)', fullfile(pwd, data_dir))
     end
-    if ~exist('../result','dir')
+
+    if ~exist('../result', 'dir')
         mkdir('../result')
     end
 
     addpath(data_dir);
+
     if strcmp(dataname, 'flickr')
         load('mir_cnn.mat');
     elseif strcmp(dataname, 'nuswide')
@@ -27,7 +29,7 @@ function [] = demo_SMFH(bits, dataname)
     gamma = 1;
     lambda = 0.01;
 
-    k_nn = 5;       %5
+    k_nn = 5; %5
 
     %% centralization
     %fprintf('centralizing data...\n');
@@ -41,32 +43,32 @@ function [] = demo_SMFH(bits, dataname)
     T_tr = bsxfun(@minus, T_tr, mean(T_tr, 1));
 
     %% mixed graph regularization term
-    W_img = adjacency(I_tr,'nn',k_nn);   % model the intra-modal similarity in image modality
-    W_txt = adjacency(T_tr,'nn',k_nn);   % model the intra-modal similarity in text modality
-    W_inter = L_tr * L_tr';        % model the label cosistency between the image and text modality
+    W_img = adjacency(I_tr, 'nn', k_nn); % model the intra-modal similarity in image modality
+    W_txt = adjacency(T_tr, 'nn', k_nn); % model the intra-modal similarity in text modality
+    W_inter = L_tr * L_tr'; % model the label cosistency between the image and text modality
     clear L_tr;
 
     [row, col] = size(I_tr);
-    D_img = zeros(row,row);
-    D_txt = zeros(row,row);
-    D_inter = zeros(row,row);
+    D_img = zeros(row, row);
+    D_txt = zeros(row, row);
+    D_inter = zeros(row, row);
 
-    W_img(W_img~=0) = 1;
-    W_txt(W_txt~=0) = 1;
+    W_img(W_img ~= 0) = 1;
+    W_txt(W_txt ~= 0) = 1;
 
     for i = 1:row
-        D_img(i,i) = sum(W_img(i,:));     
-        D_txt(i,i) = sum(W_txt(i,:));
-        D_inter(i,i) = sum(W_inter(i,:));
+        D_img(i, i) = sum(W_img(i, :));
+        D_txt(i, i) = sum(W_txt(i, :));
+        D_inter(i, i) = sum(W_inter(i, :));
     end
 
-    L_i = D_img-W_img;
+    L_i = D_img - W_img;
     clear D_img W_img;
 
-    L_t = D_txt-W_txt;
+    L_t = D_txt - W_txt;
     clear D_txt W_txt;
 
-    L_inter = D_inter-W_inter;
+    L_inter = D_inter - W_inter;
     clear D_inter W_inter;
 
     L = L_i + L_t + L_inter;
@@ -75,8 +77,8 @@ function [] = demo_SMFH(bits, dataname)
     I_temp = I_tr';
     T_temp = T_tr';
 
-    I_temp = bsxfun(@minus,I_temp , mean(I_temp,2));
-    T_temp = bsxfun(@minus,T_temp, mean(T_temp,2));
+    I_temp = bsxfun(@minus, I_temp, mean(I_temp, 2));
+    T_temp = bsxfun(@minus, T_temp, mean(T_temp, 2));
 
     Im_te = (bsxfun(@minus, I_te', mean(I_tr', 2)))';
     Te_te = (bsxfun(@minus, T_te', mean(T_tr', 2)))';
@@ -90,11 +92,11 @@ function [] = demo_SMFH(bits, dataname)
     [U1, U2, P1, P2, S] = solveSMFH(I_temp, T_temp, L, alpha, beta, gamma, lambda, bits);
 
     %% calculate hash codes
-    Yi_te = sign((bsxfun(@minus,P1 * Im_te' , mean(S,2)))');
-    Yi_db = sign((bsxfun(@minus,P1 * Im_db' , mean(S,2)))');
+    Yi_te = sign((bsxfun(@minus, P1 * Im_te', mean(S, 2)))');
+    Yi_db = sign((bsxfun(@minus, P1 * Im_db', mean(S, 2)))');
 
-    Yt_te = sign((bsxfun(@minus,P2 * Te_te' , mean(S,2)))');
-    Yt_db = sign((bsxfun(@minus,P2 * Te_db' , mean(S,2)))');
+    Yt_te = sign((bsxfun(@minus, P2 * Te_te', mean(S, 2)))');
+    Yt_db = sign((bsxfun(@minus, P2 * Te_db', mean(S, 2)))');
 
     %add by zhangzhen
     Yi_te = Yi_te > 0;
@@ -105,9 +107,9 @@ function [] = demo_SMFH(bits, dataname)
     %% evaluate
     %fprintf('start evaluating...\n');
     cbTest_vis = compactbit(Yi_te(:, 1:bits));
-    cbDb_text  = compactbit(Yt_db(:, 1:bits));
+    cbDb_text = compactbit(Yt_db(:, 1:bits));
 
-    cbTest_text  = compactbit(Yt_te(:, 1:bits));
+    cbTest_text = compactbit(Yt_te(:, 1:bits));
     cbDb_vis = compactbit(Yi_db(:, 1:bits));
 
     hamm_T2I = hammingDist(cbTest_text, cbDb_vis)';
@@ -115,11 +117,10 @@ function [] = demo_SMFH(bits, dataname)
 
     hamm_I2T = hammingDist(cbTest_vis, cbDb_text)';
     MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
-        
-	name = ['../result/' dataname '.txt'];
+
+    name = ['../result/' dataname '.txt'];
     fid = fopen(name, 'a+');
     fprintf(fid, '[%s-%d] MAP@I2T = %.4f, MAP@T2I = %.4f\n', dataname, bits, MAP_I2T, MAP_T2I);
-	fclose(fid);
+    fclose(fid);
 
 end
-
--- SMFH/code/solveSMFH.m
+++ SMFH/code/solveSMFH.m
@@ -1,61 +1,62 @@
-function [ U1, U2, P1, P2, S ] = solveSMFH( X1, X2, L, alpha, beta, gamma, lambda, bits )
-%% Reference:
-% Jun Tang, Ke Wang, Ling Shao
-% "Supervised Matrix Factorization Hashing for Cross-Modal Retrieval"
-% IEEE Transaction on Image Processing (TIP)
-% (Manuscript)
-%
-% Version1.0 -- June/2016
-% Written by Jun Tang(tangjunahu@163.com) and Ke Wang(wangke4747@126.com)
-%
-%
+function [U1, U2, P1, P2, S] = solveSMFH(X1, X2, L, alpha, beta, gamma, lambda, bits)
+    %% Reference:
+    % Jun Tang, Ke Wang, Ling Shao
+    % "Supervised Matrix Factorization Hashing for Cross-Modal Retrieval"
+    % IEEE Transaction on Image Processing (TIP)
+    % (Manuscript)
+    %
+    % Version1.0 -- June/2016
+    % Written by Jun Tang(tangjunahu@163.com) and Ke Wang(wangke4747@126.com)
+    %
+    %
 
-%% random initialization
-[row1, col1] = size(X1);
-[row2, col2] = size(X2);
-S = rand(bits, col1);
-U1 = rand(row1, bits);
-U2 = rand(row2, bits);
-P1 = rand(bits, row1);
-P2 = rand(bits, row2);
-threshold = 1;
-lastF = 99999999;
-iter = 1;
+    %% random initialization
+    [row1, col1] = size(X1);
+    [row2, col2] = size(X2);
+    S = rand(bits, col1);
+    U1 = rand(row1, bits);
+    U2 = rand(row2, bits);
+    P1 = rand(bits, row1);
+    P2 = rand(bits, row2);
+    threshold = 1;
+    lastF = 99999999;
+    iter = 1;
 
-%% compute iteratively
-while (true)
-	% update U1 and U2
-    U1 = X1 * S' / (S * S' + (lambda / alpha) * eye(bits));
-    U2 = X2 * S' / (S * S' + (lambda / (1-alpha)) * eye(bits));
-    
-    % update S    
-    A = 2 * (alpha * U1' * U1 + (1 - alpha) * U2' * U2 + 2 * beta * eye(bits) + lambda * eye(bits));
-    B = gamma * (L + L');
-    C = -2 * (alpha * U1' * X1 + (1 - alpha) * U2' * X2 + beta * (P1 * X1 + P2 * X2));
-    S = lyap(A,B,C); 
-    
-    %update P1 and P2
-    P1 = S * X1' / (X1 * X1' + (lambda / beta) * eye(row1));
-    P2 = S * X2' / (X2 * X2' + (lambda / beta) * eye(row2));
-    
-    % compute objective function
-    norm1 = alpha * (norm(X1 - U1 * S, 'fro')^2);
-    norm2 = (1 - alpha) * (norm(X2 - U2 * S, 'fro')^2);
-    norm3 = beta * (norm(S - P1 * X1, 'fro')^2);
-    norm4 = beta * (norm(S - P2 * X2, 'fro')^2);
-    norm5 = gamma * trace(S * L * S');
-    norm6 = lambda * (norm(U1, 'fro')^2 + norm(U2, 'fro')^2 + norm(S, 'fro')^2 + norm(P1, 'fro')^2 + norm(P2, 'fro')^2);
-    currentF= norm1 + norm2 + norm3 + norm4 + norm5 + norm6;
-    %fprintf('\nobj at iteration %d: %.4f\n reconstruction error for collective matrix factorization: %.4f,\n reconstruction error for linear projection: %.4f,\n joint graph term: %.4f,\n regularization term: %.4f\n\n', iter, currentF, norm1 + norm2, norm3 + norm4, norm5, norm6);
-    if (lastF - currentF) < threshold
-        %fprintf('algorithm converges...\n');
-        %fprintf('final obj: %.4f\n reconstruction error for collective matrix factorization: %.4f,\n reconstruction error for linear projection: %.4f,\n joint graph term: %.4f,\n regularization term: %.4f\n\n', currentF,norm1 + norm2, norm3 + norm4, norm5, norm6);
-        return;
+    %% compute iteratively
+    while (true)
+        % update U1 and U2
+        U1 = X1 * S' / (S * S' + (lambda / alpha) * eye(bits));
+        U2 = X2 * S' / (S * S' + (lambda / (1 - alpha)) * eye(bits));
+
+        % update S
+        A = 2 * (alpha * U1' * U1 + (1 - alpha) * U2' * U2 + 2 * beta * eye(bits) + lambda * eye(bits));
+        B = gamma * (L + L');
+        C = -2 * (alpha * U1' * X1 + (1 - alpha) * U2' * X2 + beta * (P1 * X1 + P2 * X2));
+        S = lyap(A, B, C);
+
+        %update P1 and P2
+        P1 = S * X1' / (X1 * X1' + (lambda / beta) * eye(row1));
+        P2 = S * X2' / (X2 * X2' + (lambda / beta) * eye(row2));
+
+        % compute objective function
+        norm1 = alpha * (norm(X1 - U1 * S, 'fro')^2);
+        norm2 = (1 - alpha) * (norm(X2 - U2 * S, 'fro')^2);
+        norm3 = beta * (norm(S - P1 * X1, 'fro')^2);
+        norm4 = beta * (norm(S - P2 * X2, 'fro')^2);
+        norm5 = gamma * trace(S * L * S');
+        norm6 = lambda * (norm(U1, 'fro')^2 + norm(U2, 'fro')^2 + norm(S, 'fro')^2 + norm(P1, 'fro')^2 + norm(P2, 'fro')^2);
+        currentF = norm1 + norm2 + norm3 + norm4 + norm5 + norm6;
+        %fprintf('\nobj at iteration %d: %.4f\n reconstruction error for collective matrix factorization: %.4f,\n reconstruction error for linear projection: %.4f,\n joint graph term: %.4f,\n regularization term: %.4f\n\n', iter, currentF, norm1 + norm2, norm3 + norm4, norm5, norm6);
+        if (lastF - currentF) < threshold
+            %fprintf('algorithm converges...\n');
+            %fprintf('final obj: %.4f\n reconstruction error for collective matrix factorization: %.4f,\n reconstruction error for linear projection: %.4f,\n joint graph term: %.4f,\n regularization term: %.4f\n\n', currentF,norm1 + norm2, norm3 + norm4, norm5, norm6);
+            return;
+        end
+
+        %fprintf('iter %d, loss = %d\n', iter, lastF - currentF);
+        iter = iter + 1;
+        lastF = currentF;
     end
-    %fprintf('iter %d, loss = %d\n', iter, lastF - currentF);
-    iter = iter + 1;
-    lastF = currentF;
-end
-return;
-end
 
+    return;
+end
