f3a589c4920373687603a6627f064820a3ca2a87
FutureTwT <1131797361@qq.com>
2021-11-25 09:22:18 +0800

    format LEMON

--- LEMON/code/demo_LEMON.m
+++ LEMON/code/demo_LEMON.m
@@ -1,15 +1,17 @@
-
 function [] = demo_LEMON(bits, dataname)
     data_dir = '../../Data';
-    if ~exist(data_dir,'dir')
+
+    if ~exist(data_dir, 'dir')
         error('No such dir(%s)', fullfile(pwd, data_dir))
     end
-    if ~exist('../result','dir')
+
+    if ~exist('../result', 'dir')
         mkdir('../result')
     end
 
     addpath(data_dir);
-	bits = str2num(bits);
+    bits = str2num(bits);
+
     if strcmp(dataname, 'flickr')
         load('mir_cnn.mat');
     elseif strcmp(dataname, 'nuswide')
@@ -33,24 +35,23 @@ function [] = demo_LEMON(bits, dataname)
     % X:text    Y:img
     OURparam.nbits = bits;
     GTrain_new = NormalizeFea(L_tr, 1);
-    
-    [BB,XW,YW,HH] = train_LEMON0(I_tr, T_tr, L_tr, GTrain_new, OURparam);
-    cbTest_img = compactbit(I_te*XW>0);
-    cbTest_text = compactbit(T_te*YW>0);
 
-    cbDb_img = compactbit(I_db*XW>0);
-    cbDb_text = compactbit(T_db*YW>0);
+    [BB, XW, YW, HH] = train_LEMON0(I_tr, T_tr, L_tr, GTrain_new, OURparam);
+    cbTest_img = compactbit(I_te * XW > 0);
+    cbTest_text = compactbit(T_te * YW > 0);
+
+    cbDb_img = compactbit(I_db * XW > 0);
+    cbDb_text = compactbit(T_db * YW > 0);
 
     hamm_T2I = hammingDist(cbTest_text, cbDb_img)';
     MAP_T2I = perf_metric4Label(L_db, L_te, hamm_T2I);
 
     hamm_I2T = hammingDist(cbTest_img, cbDb_text)';
     MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
-    
-	name = ['../result/' dataname '.txt'];
+
+    name = ['../result/' dataname '.txt'];
     fid = fopen(name, 'a+');
     fprintf(fid, '[%s-%d] MAP@I2T = %.4f, MAP@T2I = %.4f\n', dataname, bits, MAP_I2T, MAP_T2I);
-	fclose(fid);
+    fclose(fid);
 
 end
-
