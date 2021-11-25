a7094bb7e73769c9f11f79d6655ba10e513c4460
FutureTwT <1131797361@qq.com>
2021-11-25 09:23:16 +0800

    format LSSH

--- LSSH/code/demo_LSSH.m
+++ LSSH/code/demo_LSSH.m
@@ -1,15 +1,17 @@
-
 function [] = demo_LSSH(bits, dataname)
     addpath(genpath('./SLEP_package_4.1/SLEP/'));
     data_dir = '../../Data';
-    if ~exist(data_dir,'dir')
+
+    if ~exist(data_dir, 'dir')
         error('No such dir(%s)', fullfile(pwd, data_dir))
     end
-    if ~exist('../result','dir')
+
+    if ~exist('../result', 'dir')
         mkdir('../result')
     end
-    
+
     addpath(data_dir)
+
     if strcmp(dataname, 'flickr')
         load('mir_cnn.mat');
     elseif strcmp(dataname, 'nuswide')
@@ -21,11 +23,11 @@ function [] = demo_LSSH(bits, dataname)
     end
 
     addpath(data_dir);
-	bits = str2num(bits);
+    bits = str2num(bits);
     mu = 0.05;
     rho = 0.5;
     lambda = 0.2;
-    
+
     I_te = bsxfun(@minus, I_te, mean(I_tr, 1));
     I_db = bsxfun(@minus, I_db, mean(I_tr, 1));
     I_tr = bsxfun(@minus, I_tr, mean(I_tr, 1));
@@ -37,11 +39,11 @@ function [] = demo_LSSH(bits, dataname)
     %     % construct training set
     I_temp = I_tr';
     T_temp = T_tr';
-    [row, col]= size(I_temp);
+    [row, col] = size(I_temp);
     [rowt, colt] = size(T_temp);
 
-    I_temp = bsxfun(@minus,I_temp , mean(I_temp,2));
-    T_temp = bsxfun(@minus,T_temp, mean(T_temp,2));
+    I_temp = bsxfun(@minus, I_temp, mean(I_temp, 2));
+    T_temp = bsxfun(@minus, T_temp, mean(T_temp, 2));
     Im_te = (bsxfun(@minus, I_te', mean(I_tr', 2)))';
     Te_te = (bsxfun(@minus, T_te', mean(T_tr', 2)))';
 
@@ -52,21 +54,21 @@ function [] = demo_LSSH(bits, dataname)
     opts.rho = rho;
     opts.lambda = lambda;
     opts.maxOutIter = 20;
-    
-    [B, PX, PT, R, A, S, opts]= solveLSSH(I_temp',T_temp',bits,opts);%����LSSH��������
 
-    [train_hash,test_hash_I, test_hash_T] = LSSHcoding(A, B, PX, PT, R, I_db,T_db, Im_te,Te_te,opts, bits);
+    [B, PX, PT, R, A, S, opts] = solveLSSH(I_temp', T_temp', bits, opts); %����LSSH��������
+
+    [train_hash, test_hash_I, test_hash_T] = LSSHcoding(A, B, PX, PT, R, I_db, T_db, Im_te, Te_te, opts, bits);
     train_hash = train_hash > 0;
     train_hash = train_hash';
     cbDb_comm = compactbit(train_hash(:, 1:bits));
 
     test_hash_T = test_hash_T > 0;
     test_hash_T = test_hash_T';
-    cbTest_text  = compactbit(test_hash_T(:, 1:bits));
+    cbTest_text = compactbit(test_hash_T(:, 1:bits));
 
     test_hash_I = test_hash_I > 0;
     test_hash_I = test_hash_I';
-    cbTest_img  = compactbit(test_hash_I(:, 1:bits));
+    cbTest_img = compactbit(test_hash_I(:, 1:bits));
 
     hamm_T2I = hammingDist(cbTest_text, cbDb_comm)';
     MAP_T2I = perf_metric4Label(L_db, L_te, hamm_T2I);
@@ -74,10 +76,9 @@ function [] = demo_LSSH(bits, dataname)
     hamm_I2T = hammingDist(cbTest_img, cbDb_comm)';
     MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
 
-	name = ['../result/' dataname '.txt'];
+    name = ['../result/' dataname '.txt'];
     fid = fopen(name, 'a+');
     fprintf(fid, '[%s-%d] MAP@I2T = %.4f, MAP@T2I = %.4f\n', dataname, bits, MAP_I2T, MAP_T2I);
-	fclose(fid);
+    fclose(fid);
 
 end
-
--- LSSH/code/solveLSSH.m
+++ LSSH/code/solveLSSH.m
@@ -1,155 +1,155 @@
-function [B,PX,PT,R,A,S,opts]= solveLSSH(Xtraining,Ytraining,hash_bits,opts)
-% ||X-BS||^2 + rho*sum{|s|} + lambda*||Y - RS||^2+ mu*||T-AY||^2
-% s.t. ||B||,||R||,||A|| <=1 
-%   opts.mu = mu;
-%   opts.rho = rho;
-%   opts.lambda = lambda;
-%   opts.maxOutIter = 20;
-%Input:
-%Í¼ÏñÑµÁ·¼¯£¬ÎÄ±¾ÑµÁ·¼¯£¬¹þÏ£Âë³¤¶È£¬²ÎÊý£ºmu,lambda,rho,maxOutIter
-%Output:
-%Í¼ÏñµÄÍêÈ«»ù¼¯ºÏB£¬Ï¡ÊèÏ¡ÊèS£»ÎÄ±¾µÄÇ±ÔÚÓïÒåA£»
+function [B, PX, PT, R, A, S, opts] = solveLSSH(Xtraining, Ytraining, hash_bits, opts)
+    % ||X-BS||^2 + rho*sum{|s|} + lambda*||Y - RS||^2+ mu*||T-AY||^2
+    % s.t. ||B||,||R||,||A|| <=1
+    %   opts.mu = mu;
+    %   opts.rho = rho;
+    %   opts.lambda = lambda;
+    %   opts.maxOutIter = 20;
+    %Input:
+    %Í¼ï¿½ï¿½Ñµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä±ï¿½Ñµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ï£ï¿½ë³¤ï¿½È£ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½mu,lambda,rho,maxOutIter
+    %Output:
+    %Í¼ï¿½ï¿½ï¿½ï¿½ï¿½È«ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Bï¿½ï¿½Ï¡ï¿½ï¿½Ï¡ï¿½ï¿½Sï¿½ï¿½ï¿½Ä±ï¿½ï¿½ï¿½Ç±ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Aï¿½ï¿½
 
+    % Reference:
+    % Jile Zhou, GG Ding, Yuchen Guo
+    % "Latent Semantic Sparse Hashing for Cross-modal Similarity Search"
+    % ACM SIGIR 2014
+    % (Manuscript)
+    %
+    % Version1.0 -- Nov/2013
+    % Written by Jile Zhou (zhoujile539@gmail.com), Yuchen Guo (yuchen.w.guo@gmail.com)
+    %
 
-% Reference:
-% Jile Zhou, GG Ding, Yuchen Guo
-% "Latent Semantic Sparse Hashing for Cross-modal Similarity Search"
-% ACM SIGIR 2014
-% (Manuscript)
-%
-% Version1.0 -- Nov/2013
-% Written by Jile Zhou (zhoujile539@gmail.com), Yuchen Guo (yuchen.w.guo@gmail.com)
-%
+    warning off all
+    addpath('./SLEP_package_4.1/SLEP/');
+    addpath('./SLEP_package_4.1/SLEP/functions/L1/L1R');
+    randNum = 1;
+    %ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¸ï¿½ï¿½ï¿½
+    nSamples = 10000;
 
-warning off all
-addpath('./SLEP_package_4.1/SLEP/');
-addpath('./SLEP_package_4.1/SLEP/functions/L1/L1R');
-randNum=1;
-%ÉèÖÃÑù±¾µãµÄ¸öÊý
-nSamples = 10000;
-if nSamples == size(Xtraining,1)
-    training_id = 1:nSamples;
-elseif (nSamples < size(Xtraining,1))
-    fprintf('Training model by sampling %d points randomly\n',nSamples);
-    training_id = randperm(size(Xtraining,1));
-    training_id = training_id(1:nSamples);
-else
-    fprintf('Training model by using the whole %d training data\n',size(Xtraining,1));
-    training_id = 1:size(Xtraining,1);
-    nSamples = size(Xtraining, 1);
-end
-%%construct data
-low_dim = hash_bits;
-X = Xtraining(training_id, :)';
-T = Ytraining(training_id, :)';
+    if nSamples == size(Xtraining, 1)
+        training_id = 1:nSamples;
+    elseif (nSamples < size(Xtraining, 1))
+        fprintf('Training model by sampling %d points randomly\n', nSamples);
+        training_id = randperm(size(Xtraining, 1));
+        training_id = training_id(1:nSamples);
+    else
+        fprintf('Training model by using the whole %d training data\n', size(Xtraining, 1));
+        training_id = 1:size(Xtraining, 1);
+        nSamples = size(Xtraining, 1);
+    end
 
-% ÖÐÐÄ»¯Êý¾Ý£¬The data matrix is of size m x n
-X = bsxfun(@minus,X, mean( (Xtraining'),2));% X in the paper
-T = bsxfun(@minus,T, mean( (Ytraining'),2));% Y in the paper
-%%% PCA %%%%%
-% [U,S,V] = svd(cov(X'));
-[U, ~] =  eigs(cov(X'), low_dim);%pca is for row data
-PX = U(:,1:low_dim);%ÎªÁË¶ÔÊý¾Ý½øÐÐÖ÷³É·Ý·ÖÎö
-% PX = eye(size(X, 1));
-X = PX'* X;
+    %%construct data
+    low_dim = hash_bits;
+    X = Xtraining(training_id, :)';
+    T = Ytraining(training_id, :)';
 
- %U =  pca(T');%pca is for row data
-%[U, ~] =  eigs(cov(T'), low_dim);%pca is for row data
-%PT = U(:,1:low_dim);
-PT = eye(size(T, 1));
+    % ï¿½ï¿½ï¿½Ä»ï¿½ï¿½ï¿½ï¿½Ý£ï¿½The data matrix is of size m x n
+    X = bsxfun(@minus, X, mean((Xtraining'), 2)); % X in the paper
+    T = bsxfun(@minus, T, mean((Ytraining'), 2)); % Y in the paper
+    %%% PCA %%%%%
+    % [U,S,V] = svd(cov(X'));
+    [U, ~] = eigs(cov(X'), low_dim); %pca is for row data
+    PX = U(:, 1:low_dim); %Îªï¿½Ë¶ï¿½ï¿½ï¿½ï¿½Ý½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½É·Ý·ï¿½ï¿½ï¿½
+    % PX = eye(size(X, 1));
+    X = PX' * X;
 
-T = PT'* T;%%TÃ»·¢Éú±ä»¯¡££¿ÎªÊ²Ã´Òª×öÕâÒ»²½´¦ÀíÄØ£¿ 
+    %U =  pca(T');%pca is for row data
+    %[U, ~] =  eigs(cov(T'), low_dim);%pca is for row data
+    %PT = U(:,1:low_dim);
+    PT = eye(size(T, 1));
 
-%P = eye(size(X,1));
-%% preprocessingData ¶ÔÊý¾Ý½øÐÐ¹éÒ»»¯´¦Àí
-norm_y = sum(X.^2,1).^-.5;
-for i = 1 : size(X,1)
-    X(i,:) = X(i,:).*norm_y;
-end
+    T = PT' * T; % %TÃ»ï¿½ï¿½ï¿½ï¿½ï¿½ä»¯ï¿½ï¿½ï¿½ï¿½ÎªÊ²Ã´Òªï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ø£ï¿½
 
-norm_t = sum(T.^2,1).^-.5;
-for i = 1 : size(T,1)
-    T(i,:) = T(i,:).*norm_t;
-end
+    %P = eye(size(X,1));
+    %% preprocessingData ï¿½ï¿½ï¿½ï¿½ï¿½Ý½ï¿½ï¿½Ð¹ï¿½Ò»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+    norm_y = sum(X.^2, 1).^ - .5;
 
-%% ³õÊ¼»¯¸÷¸ö±äÁ¿
-%dim0 : sourse data dimention
-%dim1 : sparse coding dimention
-[dim0 num] = size(X);
-dim1 = 512;%¶¨ÒåB£¬RµÄÁÐÊý
-randn('state',(randNum-1)*3+1);
-A=randn(size(T, 1),hash_bits);       % the data matrix£¬
-B=randn(dim0,dim1);
-R=randn(hash_bits,dim1);
+    for i = 1:size(X, 1)
+        X(i, :) = X(i, :) .* norm_y;
+    end
 
-%% DFF»ñÈ¡²ÎÊý
-rho = opts.rho;  % the regularization parameter
-mu = opts.mu;% mu in the paper
-lambda = opts.lambda;% gama in the paper 
-%rho=0.6;            % the regularization parameter
+    norm_t = sum(T.^2, 1).^ - .5;
 
-% it is a ratio between (0,1), if .rFlag=1
+    for i = 1:size(T, 1)
+        T(i, :) = T(i, :) .* norm_t;
+    end
 
-%----------------------- Set optional items ------------------------
+    %% ï¿½ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+    %dim0 : sourse data dimention
+    %dim1 : sparse coding dimention
+    [dim0 num] = size(X);
+    dim1 = 512; %ï¿½ï¿½ï¿½ï¿½Bï¿½ï¿½Rï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+    randn('state', (randNum - 1) * 3 + 1);
+    A = randn(size(T, 1), hash_bits); % the data matrixï¿½ï¿½
+    B = randn(dim0, dim1);
+    R = randn(hash_bits, dim1);
 
+    %% DFFï¿½ï¿½È¡ï¿½ï¿½ï¿½ï¿½
+    rho = opts.rho; % the regularization parameter
+    mu = opts.mu; % mu in the paper
+    lambda = opts.lambda; % gama in the paper
+    %rho=0.6;            % the regularization parameter
 
-% Starting point
-opts.init=2;        % starting from a zero point
+    % it is a ratio between (0,1), if .rFlag=1
 
-% termination criterion
-opts.tFlag=5;       % run .maxIter iterations
-opts.maxIter=30;   % maximum number of iterations
+    %----------------------- Set optional items ------------------------
 
-% normalization
-opts.nFlag=0;       % without normalization
+    % Starting point
+    opts.init = 2; % starting from a zero point
 
-% regularization
-opts.rFlag=1;       % the input parameter 'rho' is a ratio in (0, 1)
-%opts.rsL2=0.01;     % the squared two norm term
-%opts.lambda = 0.1;
-t = 0 ; % out-loop itration
-maxIter = opts.maxOutIter; % out-loop itration
-opts.mFlag=0;       % treating it as compositive function
-opts.lFlag=0;       % Nemirovski's line search
+    % termination criterion
+    opts.tFlag = 5; % run .maxIter iterations
+    opts.maxIter = 30; % maximum number of iterations
 
-%----------------------- Run the code LeastR -----------------------
-S = zeros(size(B,2),size(X,2));
-%fprintf('ScmHashing initing: dim = %d, bits = %d, bases = %d, lambda = %s, sparse = %s, maxIter = %d \n',size(X,1),hash_bits,size(B,2),num2str(opts.lambda),num2str(rho),maxIter);
-% addAttachedFiles(pool, LeastR)
-for i = 1 : size(X,2)
-    %if (mod(i,1000)==0)
-    %    fprintf('.');
-    %end
-    [S(:,i), ~, ~]= LeastR(B, X(:,i), rho, opts);
-end
-%fprintf('\n');
-while t < maxIter
-    %tic;
-    Y = (A'*A + lambda/mu *eye(hash_bits))\(lambda/mu*R*S + A'*T);% A in the paper
-    %Y = R*S*(1/opts.lambda);
-    
-    
-    
-    A = l2ls_learn_basis_dual(T, Y, 1);% U in the paper
-    %A = X*Y'/(Y*Y' + mu*eye(size(Y,1)));
-for i = 1 : size(X,2)
-    [S(:,i), ~, ~]= LeastR([B;sqrt(lambda) * R], [X(:,i);sqrt(lambda) * Y(:,i)], rho, opts);
- end
-    
-    B = l2ls_learn_basis_dual(X, S, 1);%ÎÄÖÐµÄB
-    R = l2ls_learn_basis_dual(Y, S, 1);%R in the paper
-    
-    sparse_error = sum(sum((X-B*S).^2));%ÀûÓÃÏ¡Êè±àÂëÇóÍ¼ÏñµÄÏ¡Êè±íÊ¾
-    sparse_embdding_error = sum(sum((Y-R*S).^2));%±íÊ¾Á½ÖÖÄ£Ì¬Ö®¼äµÄ¹ØÁª
-    matrix_factrozation_error = sum(sum((T-A*Y).^2));%ÀûÓÃ¾ØÕó·Ö½âÇóÎÄ±¾µÄÇ±ÔÚÓïÒå
-    obj = sparse_error + rho* sum(sum(abs(S))) + opts.lambda*sparse_embdding_error + matrix_factrozation_error;% + mu*(sum(sum(B.^2))+sum(sum(R.^2))+sum(sum(A.^2)));
-    t = t + 1;
-    %fprintf('%d/%d avgSpErr: %s, avgSpEmErr: %s, avgMfErr: %s, obj = %s, sparse = %.4f\n',t,maxIter,...
-    %    num2str(sparse_error/size(X,2)),num2str(sparse_embdding_error/size(X,2)),num2str(matrix_factrozation_error/size(X,2)),num2str(obj), length(find(S == 0)) / nSamples / dim1);
-    %toc;
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% coding
+    % normalization
+    opts.nFlag = 0; % without normalization
 
-%save('gist_SpH_matrix_1024_itr25_64_lambda_01','train_hash','test_hash');
+    % regularization
+    opts.rFlag = 1; % the input parameter 'rho' is a ratio in (0, 1)
+    %opts.rsL2=0.01;     % the squared two norm term
+    %opts.lambda = 0.1;
+    t = 0; % out-loop itration
+    maxIter = opts.maxOutIter; % out-loop itration
+    opts.mFlag = 0; % treating it as compositive function
+    opts.lFlag = 0; % Nemirovski's line search
 
+    %----------------------- Run the code LeastR -----------------------
+    S = zeros(size(B, 2), size(X, 2));
+    %fprintf('ScmHashing initing: dim = %d, bits = %d, bases = %d, lambda = %s, sparse = %s, maxIter = %d \n',size(X,1),hash_bits,size(B,2),num2str(opts.lambda),num2str(rho),maxIter);
+    % addAttachedFiles(pool, LeastR)
+    for i = 1:size(X, 2)
+        %if (mod(i,1000)==0)
+        %    fprintf('.');
+        %end
+        [S(:, i), ~, ~] = LeastR(B, X(:, i), rho, opts);
+    end
 
+    %fprintf('\n');
+    while t < maxIter
+        %tic;
+        Y = (A' * A + lambda / mu * eye(hash_bits)) \ (lambda / mu * R * S + A' * T); % A in the paper
+        %Y = R*S*(1/opts.lambda);
+
+        A = l2ls_learn_basis_dual(T, Y, 1); % U in the paper
+        %A = X*Y'/(Y*Y' + mu*eye(size(Y,1)));
+        for i = 1:size(X, 2)
+            [S(:, i), ~, ~] = LeastR([B; sqrt(lambda) * R], [X(:, i); sqrt(lambda) * Y(:, i)], rho, opts);
+        end
+
+        B = l2ls_learn_basis_dual(X, S, 1); %ï¿½ï¿½ï¿½Ðµï¿½B
+        R = l2ls_learn_basis_dual(Y, S, 1); %R in the paper
+
+        sparse_error = sum(sum((X - B * S).^2)); %ï¿½ï¿½ï¿½ï¿½Ï¡ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Í¼ï¿½ï¿½ï¿½Ï¡ï¿½ï¿½ï¿½Ê¾
+        sparse_embdding_error = sum(sum((Y - R * S).^2)); %ï¿½ï¿½Ê¾ï¿½ï¿½ï¿½ï¿½Ä£Ì¬Ö®ï¿½ï¿½Ä¹ï¿½ï¿½ï¿½
+        matrix_factrozation_error = sum(sum((T - A * Y).^2)); %ï¿½ï¿½ï¿½Ã¾ï¿½ï¿½ï¿½Ö½ï¿½ï¿½ï¿½ï¿½Ä±ï¿½ï¿½ï¿½Ç±ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+        obj = sparse_error + rho * sum(sum(abs(S))) + opts.lambda * sparse_embdding_error + matrix_factrozation_error; % + mu*(sum(sum(B.^2))+sum(sum(R.^2))+sum(sum(A.^2)));
+        t = t + 1;
+        %fprintf('%d/%d avgSpErr: %s, avgSpEmErr: %s, avgMfErr: %s, obj = %s, sparse = %.4f\n',t,maxIter,...
+        %    num2str(sparse_error/size(X,2)),num2str(sparse_embdding_error/size(X,2)),num2str(matrix_factrozation_error/size(X,2)),num2str(obj), length(find(S == 0)) / nSamples / dim1);
+        %toc;
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% coding
+
+    %save('gist_SpH_matrix_1024_itr25_64_lambda_01','train_hash','test_hash');
