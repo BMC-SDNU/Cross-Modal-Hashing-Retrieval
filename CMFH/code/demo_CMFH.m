eb2c64f45eb8caea5cfa37c5c5cbc9f3df3eccd0
FutureTwT <1131797361@qq.com>
2021-11-25 09:13:50 +0800

    format CMFH

--- CMFH/code/demo_CMFH.m
+++ CMFH/code/demo_CMFH.m
@@ -1,17 +1,18 @@
-
 function [] = demo_CMFH(bits, dataname)
     %addpath('../common');
     data_dir = '../../Data';
-    if ~exist(data_dir,'dir')
+
+    if ~exist(data_dir, 'dir')
         error('No such dir(%s)', fullfile(pwd, data_dir))
     end
-    if ~exist('../result','dir')
+
+    if ~exist('../result', 'dir')
         mkdir('../result')
     end
 
     addpath(data_dir);
 
-	bits = str2num(bits);
+    bits = str2num(bits);
 
     CMFHparam.alphas = [0.5 0.5];
     CMFHparam.gamma = 0.01;
@@ -26,6 +27,7 @@ function [] = demo_CMFH(bits, dataname)
     else
         fprintf('ERROR dataname!');
     end
+
     %% centralization
     %fprintf('centralizing data...\n');
     I_te = bsxfun(@minus, I_te, mean(I_tr, 1));
@@ -38,35 +40,34 @@ function [] = demo_CMFH(bits, dataname)
 
     I_temp = I_tr';
     T_temp = T_tr';
-    [row, col]= size(I_temp);  
+    [row, col] = size(I_temp);
     [rowt, colt] = size(T_temp);
 
-    I_temp = bsxfun(@minus,I_temp , mean(I_temp,2));
-    T_temp = bsxfun(@minus,T_temp, mean(T_temp,2));
+    I_temp = bsxfun(@minus, I_temp, mean(I_temp, 2));
+    T_temp = bsxfun(@minus, T_temp, mean(T_temp, 2));
     Im_te = (bsxfun(@minus, I_te', mean(I_temp, 2)))';
     Te_te = (bsxfun(@minus, T_te', mean(T_temp, 2)))';
 
-    [model, B]= CMFH_learn(I_temp', T_temp',CMFHparam, bits);
+    [model, B] = CMFH_learn(I_temp', T_temp', CMFHparam, bits);
     %% calculate hash codes
-     [codeDbX,codeDbY,test_hash_X, test_hash_T]=CMFH_compress1(model, I_db, T_db, I_te, T_te);
+    [codeDbX, codeDbY, test_hash_X, test_hash_T] = CMFH_compress1(model, I_db, T_db, I_te, T_te);
 
     %% evaluate
-    codeTestit=test_hash_X;
-    codeTestti=test_hash_T;
-    cbDbX = compactbit(codeDbX(:,1:bits));
-    cbDbY = compactbit(codeDbY(:,1:bits));
-    cbTestit  = compactbit(codeTestit(:,1:bits));
-    cbTestti  = compactbit(codeTestti(:,1:bits));
-    hamm_I2T  = hammingDist(cbTestit,cbDbY )';
-    hamm_T2I  = hammingDist(cbTestti,cbDbX )';
+    codeTestit = test_hash_X;
+    codeTestti = test_hash_T;
+    cbDbX = compactbit(codeDbX(:, 1:bits));
+    cbDbY = compactbit(codeDbY(:, 1:bits));
+    cbTestit = compactbit(codeTestit(:, 1:bits));
+    cbTestti = compactbit(codeTestti(:, 1:bits));
+    hamm_I2T = hammingDist(cbTestit, cbDbY)';
+    hamm_T2I = hammingDist(cbTestti, cbDbX)';
 
     MAP_T2I = perf_metric4Label(L_db, L_te, hamm_T2I);
     MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
 
-	name = ['../result/' dataname '.txt'];
+    name = ['../result/' dataname '.txt'];
     fid = fopen(name, 'a+');
     fprintf(fid, '[%s-%d] MAP@I2T = %.4f, MAP@T2I = %.4f\n', dataname, bits, MAP_I2T, MAP_T2I);
-	fclose(fid);
+    fclose(fid);
 
 end
-
--- CVH/code/demo_CVH.m
+++ CVH/code/demo_CVH.m
@@ -1,14 +1,17 @@
-
 function [] = demo_CVH(bits, dataname)
 
     data_dir = '../../Data';
-    if ~exist(data_dir,'dir')
+
+    if ~exist(data_dir, 'dir')
         error('No such dir(%s)', fullfile(pwd, data_dir))
     end
-    if ~exist('../result','dir')
+
+    if ~exist('../result', 'dir')
         mkdir('../result')
     end
+
     addpath(data_dir);
+
     if strcmp(dataname, 'flickr')
         load('mir_cnn.mat');
     elseif strcmp(dataname, 'nuswide')
@@ -20,64 +23,63 @@ function [] = demo_CVH(bits, dataname)
     end
 
     k_near = 5;
-	bits = str2num(bits);
+    bits = str2num(bits);
 
     conf.randSeed = 1;
-    randn('state',conf.randSeed) ;
-    rand('state',conf.randSeed) ;
+    randn('state', conf.randSeed);
+    rand('state', conf.randSeed);
 
+    feaTrain_text = T_tr;
+    clear T_tr;
+    feaTrain_vis = I_tr;
+    clear I_tr;
+    feaTrain_class = L_tr;
+    clear L_tr;
 
-	feaTrain_text = T_tr;
-	clear T_tr;
-	feaTrain_vis = I_tr;
-	clear I_tr;
-	feaTrain_class = L_tr;
-	clear L_tr;
+    feaTest_text = T_te;
+    clear T_te;
+    feaTest_vis = I_te;
+    clear I_te;
+    feaTest_class = L_te;
 
-	feaTest_text = T_te;
-	clear T_te;
-	feaTest_vis = I_te;
-	clear I_te;
-	feaTest_class =L_te;
+    feaDb_text = T_db;
+    clear T_db;
+    feaDb_vis = I_db;
+    clear I_db;
+    feaDb_class = L_db;
 
-	feaDb_text = T_db;
-	clear T_db;
-	feaDb_vis = I_db;
-	clear I_db;
-	feaDb_class =L_db;
+    CVHparam.lambda = 1e-4;
 
-	CVHparam.lambda=1e-4;
+    [model, drop] = CVH_learn(feaTrain_vis, feaTrain_text, CVHparam, bits);
 
-	[model, drop]=CVH_learn(feaTrain_vis , feaTrain_text,CVHparam, bits);
+    codeDb_T = CVH_compress1(feaDb_text, model);
+    codeDb_I = CVH_compress(feaDb_vis, model);
 
-	codeDb_T = CVH_compress1(feaDb_text, model);
-	codeDb_I = CVH_compress(feaDb_vis, model);
+    codeTest_T = CVH_compress1(feaTest_text, model);
+    codeTest_I = CVH_compress(feaTest_vis, model);
 
-	codeTest_T = CVH_compress1(feaTest_text, model);
-	codeTest_I = CVH_compress(feaTest_vis, model);
+    %% evaluation
+    codeDb_T = codeDb_T';
+    codeDb_I = codeDb_I';
 
-	%% evaluation
-	codeDb_T = codeDb_T';
-	codeDb_I = codeDb_I';
+    codeTest_T = codeTest_T';
+    codeTest_I = codeTest_I';
 
-	codeTest_T = codeTest_T';
-	codeTest_I = codeTest_I';
+    cbDb_T = compactbit(codeDb_T(:, 1:bits));
+    cbDb_I = compactbit(codeDb_I(:, 1:bits));
 
-	cbDb_T = compactbit(codeDb_T(:,1:bits));
-	cbDb_I = compactbit(codeDb_I(:,1:bits));
-	
-	cbTest_T = compactbit(codeTest_T(:,1:bits));
-	cbTest_I = compactbit(codeTest_I(:,1:bits));
+    cbTest_T = compactbit(codeTest_T(:, 1:bits));
+    cbTest_I = compactbit(codeTest_I(:, 1:bits));
 
-	hamm_T2I = hammingDist(cbTest_T, cbDb_I)';
-	MAP_T2I = perf_metric4Label(L_db, L_te, hamm_T2I);
+    hamm_T2I = hammingDist(cbTest_T, cbDb_I)';
+    MAP_T2I = perf_metric4Label(L_db, L_te, hamm_T2I);
 
-	hamm_I2T = hammingDist(cbTest_I, cbDb_T)';
-	MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
+    hamm_I2T = hammingDist(cbTest_I, cbDb_T)';
+    MAP_I2T = perf_metric4Label(L_db, L_te, hamm_I2T);
 
-	name = ['../result/' dataname '.txt'];
+    name = ['../result/' dataname '.txt'];
     fid = fopen(name, 'a+');
     fprintf(fid, '[%s-%d] MAP@I2T = %.4f, MAP@T2I = %.4f\n', dataname, bits, MAP_I2T, MAP_T2I);
-	fclose(fid);
+    fclose(fid);
 
 end
